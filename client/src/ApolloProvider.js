546ff0ccc8a7f0a3a329c0039766c3d7043dddb7
SunilDeveloper7 <sunnil474@gmail.com>
2021-04-18 11:05:59 -0700

    Created .env file,config and created apollo provider for my graphql

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import App from './App';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { setContext } from 'apollo-link-context';
+
+const httpLink = createHttpLink({
+uri: 'http://localhost:4000'
+});
+
+const authLink = setContext(() => {
+const token = localStorage.getItem('jwtToken');
+return {
+    headers: {
+    Authorization: token ? `Bearer ${token}` : ''
+    }
+};
+});
+
+const client = new ApolloClient({
+link: authLink.concat(httpLink),
+cache: new InMemoryCache()
+});
+
+export default (
+<ApolloProvider client={client}>
+    <App />
+</ApolloProvider>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { render } from 'react-dom';
+// import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from 'react-apollo';
+// import App from './App';
+
+// // Pass your GraphQL endpoint to uri
+// const client = new ApolloClient({
+//     uri: 'http://localhost:4000/graphql' // no error
+// });
+
+// // const client = new ApolloClient({
+// //     link:httpLink,
+// //     Cache:new InMemoryCache()
+
+// // })
+
+// export default (
+//     <ApolloProvider client={client}>
+//     <App />
+//     </ApolloProvider>
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
