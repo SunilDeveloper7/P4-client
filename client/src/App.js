0b99b5e7f4b4ae7734b69f631330ef99ba517481
SunilDeveloper7 <sunnil474@gmail.com>
2021-04-18 15:29:58 -0700

    Still working on my components

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
-import '/Users/sunillama/Assembly25/unit-4/P4/client/src/App.css';
-import {Container } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react';
 
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+import { AuthProvider } from './context/auth';
+import AuthRoute from './util/AuthRoute';
 
 import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
-import Register from './pages/Register';
 import Login from './pages/Login';
-import { contained } from 'semantic-ui-react'
-
+import Register from './pages/Register';
+import SinglePost from './pages/singlePost';
 
 function App() {
   return (
-    <Router>
-    <container>
-    <MenuBar />
-    <Route exact path='/' component={Home}/>
-    <Route exact path='/login' component={Login}/>
-    <Route exact path='/register' component={Register}/>
-    
-    </container>
-      
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
+        </Container>
       </Router>
-    
+    </AuthProvider>
   );
 }
 
--- client/src/serviceWorker.js.js
+++ client/src/serviceWorker.js.js
@@ -1,8 +1,8 @@
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
+    
     window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
+    
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
@@ -10,12 +10,10 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    // The URL constructor is available in all browsers that support SW.
+    
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      
       return;
     }
 
@@ -54,9 +52,7 @@ function registerValidSW(swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
+            
               console.log(
                 'New content is available and will be used when all ' +
                   'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
@@ -67,9 +63,7 @@ function registerValidSW(swUrl, config) {
                 config.onUpdate(registration);
               }
             } else {
-              // At this point, everything has been precached.
-              // It's the perfect time to display a
-              // "Content is cached for offline use." message.
+            
               console.log('Content is cached for offline use.');
 
               // Execute callback
