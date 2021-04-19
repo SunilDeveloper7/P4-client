d3e0c29734d7c662773db483ea7696e12410e1db
SunilDeveloper7 <sunnil474@gmail.com>
2021-04-18 19:11:05 -0700

    created my home components and menu bar from sementic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react'
 
 import 'semantic-ui-css/semantic.min.css';
-import './App.css';
-
-import { AuthProvider } from './context/auth';
-import AuthRoute from './util/AuthRoute';
-
-import MenuBar from './components/MenuBar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import SinglePost from './pages/singlePost';
+import '/Users/sunillama/Assembly25/unit-4/P4/client/src/App.css';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import MenuBar from './components/MenuBar'
+import Message from './pages/Message'
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Container>
-          <MenuBar />
-          <Route exact path="/" component={Home} />
-          <AuthRoute exact path="/login" component={Login} />
-          <AuthRoute exact path="/register" component={Register} />
-          <Route exact path="/posts/:postId" component={SinglePost} />
-        </Container>
-      </Router>
-    </AuthProvider>
+    <Router>
+    <Container>
+    <MenuBar />
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='/register' component={Register}/>
+      <Route exact path='/message' component={Message}/>
+      </Container>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Message.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Message() {
+    return (
+        <div>
+        <h1>
+        I am a message box
+        </h1>
+        </div>
+    )
+}
+export default Message;
